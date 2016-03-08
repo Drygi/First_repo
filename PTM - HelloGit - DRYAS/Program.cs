c192bdacc61be185b9c5deb67582a6196769733a
Drygi <mikolaj_drygas@tlen.pl>
2016-03-08 14:10:47 +0100

    stworzenie  helloGIT

---
+++ PTM - HelloGit - DRYAS/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PTM___HelloGit___DRYAS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HELLO WORD");
+            Console.ReadKey();
+        }
+    }
+}
